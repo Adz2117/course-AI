d05eddeae86c0670aeb2635db6bed8810787fc39
Adz2117 <110487711+Adz2117@users.noreply.github.com>
2023-03-11 12:26:56 +0330

    Add files via upload

--- exercise_session1.py
+++ exercise_session1.py
@@ -9,26 +9,27 @@ while a > 0:
     a = a - 1
     b = b - 1
     c = c + 2
+print("\n")
 print("end \n")
 
 #second
-#get a number from user and determine that is first number or not
+#get a number from user and determine that is prime number or not
 print("second :")
-a = int(input("please add your arbitrary number to determine that is first number or not : "))
+a = int(input("please add your arbitrary number to determine that is prime number or not : "))
 b = 0
 for i in range( 1 , a+1):
     if a%i == 0:
         b += 1
 
 if b == 2:
-    print(f"{a} , is a prime number.")
+    print(f"{a} , yes it is a prime number.")
 else:
-    print(f"{a} , isn't a prime number.")
-
+    print(f"{a} , no it isn't a prime number.")
+print("\n")
 print("end \n")
 
 #third
-#determine first number until 1000
+#determine prime number until 1000
 print("third :")
 for i in range(1 , 1001):
     if i > 1:
